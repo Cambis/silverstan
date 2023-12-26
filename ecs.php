55f2ec9cc120f5fdc474a411924a40396cf5dc3c
Cameron Bryers <12287346+Cambis@users.noreply.github.com>
2023-12-26 14:38:06 +1300

    :sparkles: feat: initial commit

---
+++ ecs.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
+        __DIR__ . '/rector.php',
+        __DIR__ . '/src',
+        __DIR__ . '/stubs',
+        __DIR__ . '/tests',
+    ]);
+
+    $ecsConfig->sets([
+        SetList::ARRAY,
+        SetList::CLEAN_CODE,
+        SetList::PSR_12,
+    ]);
+
+    $ecsConfig->skip([
+        '*/Fixture/*',
+        '*/Source/*',
+    ]);
+};
---
+++ rector.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->importNames();
+
+    $rectorConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+    $rectorConfig->removeUnusedImports();
+
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_81,
+        SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
+        SetList::DEAD_CODE,
+        SetList::EARLY_RETURN,
+        SetList::PRIVATIZATION,
+        PHPUnitSetList::PHPUNIT_90,
+    ]);
+
+    $rectorConfig->skip([
+        '*/Fixture/*',
+        '*/Source/*',
+        ClosureToArrowFunctionRector::class,
+    ]);
+};
