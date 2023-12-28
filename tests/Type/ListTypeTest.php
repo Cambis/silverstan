f02322faccac8f9539cc230b9eed41d72857b882
Cameron Bryers <12287346+Cambis@users.noreply.github.com>
2023-12-28 17:04:23 +1300

    :sparkles: feat: add stubs for datalist and arraylist
    - Add test cases
    - Format stubs to use fqn

---
+++ tests/Type/ListTypeTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cambis\Silverstan\Tests\Type;
+
+use Generator;
+use PHPStan\Testing\TypeInferenceTestCase;
+
+final class ListTypeTest extends TypeInferenceTestCase
+{
+    public function typeFileAsserts(): Generator
+    {
+        yield from $this->gatherAssertTypes(__DIR__ . '/Fixture/array-list-types.php.inc');
+        yield from $this->gatherAssertTypes(__DIR__ . '/Fixture/data-list-types.php.inc');
+    }
+
+    /**
+     * @dataProvider typeFileAsserts
+     */
+    public function testFileAsserts(string $assertType, string $file, mixed ...$args): void
+    {
+        $this->assertFileAsserts($assertType, $file, ...$args);
+    }
+
+    /**
+     * @return string[]
+     */
+    public static function getAdditionalConfigFiles(): array
+    {
+        return [__DIR__ . '/../../phpstan.neon.dist'];
+    }
+}
