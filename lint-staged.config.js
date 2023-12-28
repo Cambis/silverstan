a7f819e419d88deadaa04ceab22eaa816f3c6e01
Cameron Bryers <12287346+Cambis@users.noreply.github.com>
2023-12-29 11:11:24 +1300

    :hammer: chore: add php-parallel-lint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,7 @@
 /** @type {import('lint-staged').Config} */
 module.exports = {
   '*.php': [
+    'php vendor/bin/parallel-lint --colors --blame',
     'php vendor/bin/ecs check --fix --ansi',
     'php vendor/bin/phpstan analyse --ansi --memory-limit=-1',
   ],
