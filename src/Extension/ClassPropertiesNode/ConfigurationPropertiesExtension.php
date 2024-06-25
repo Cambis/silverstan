46ba228464d2525c2aff9a2fcedf94082d86ec41
Cameron Bryers <12287346+Cambis@users.noreply.github.com>
2024-06-25 21:50:42 +1200

    :art: chore: cleanup ConfigurationPropertiesExtension
    - Rename internal function
    - Remove rule doc generation, this was not working at this point anyways

--- src/Extension/ClassPropertiesNode/ConfigurationPropertiesExtension.php
+++ src/Extension/ClassPropertiesNode/ConfigurationPropertiesExtension.php
@@ -10,56 +10,29 @@ use PHPStan\Reflection\PropertyReflection;
 use PHPStan\Rules\Properties\ReadWritePropertiesExtension;
 use SilverStripe\Core\Config\Configurable;
 use SilverStripe\Core\Extension;
-use Symplify\RuleDocGenerator\Contract\DocumentedRuleInterface;
-use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
-use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 use function str_contains;
 
 /**
  * @see \Cambis\Silverstan\Tests\Extension\ClassPropertiesNode\ConfigurationPropertiesExtensionTest
  */
-final class ConfigurationPropertiesExtension implements ReadWritePropertiesExtension, DocumentedRuleInterface
+final class ConfigurationPropertiesExtension implements ReadWritePropertiesExtension
 {
-    #[Override]
-    public function getRuleDefinition(): RuleDefinition
-    {
-        return new RuleDefinition(
-            'Allows configurable read-write properties.',
-            [new CodeSample(
-                <<<'CODE_SAMPLE'
-final class Foo extends \SilverStripe\ORM\DataObject
-{
-    private array $bar = [];
-}
-CODE_SAMPLE
-                ,
-                <<<'CODE_SAMPLE'
-final class Foo extends \SilverStripe\ORM\DataObject
-{
-    private static array $bar = [];
-}
-CODE_SAMPLE
-            ),
-            ]
-        );
-    }
-
     #[Override]
     public function isAlwaysRead(PropertyReflection $propertyReflection, string $propertyName): bool
     {
-        return !$this->shouldSkipProperty($propertyReflection);
+        return $this->isConfigurationProperty($propertyReflection);
     }
 
     #[Override]
     public function isAlwaysWritten(PropertyReflection $propertyReflection, string $propertyName): bool
     {
-        return !$this->shouldSkipProperty($propertyReflection);
+        return $this->isConfigurationProperty($propertyReflection);
     }
 
     #[Override]
     public function isInitialized(PropertyReflection $propertyReflection, string $propertyName): bool
     {
-        return !$this->shouldSkipProperty($propertyReflection);
+        return $this->isConfigurationProperty($propertyReflection);
     }
 
     private function shouldSkipClass(ClassReflection $classReflection): bool
@@ -71,22 +44,22 @@ CODE_SAMPLE
         return !$classReflection->hasTraitUse(Configurable::class);
     }
 
-    private function shouldSkipProperty(PropertyReflection $propertyReflection): bool
+    private function isConfigurationProperty(PropertyReflection $propertyReflection): bool
     {
         $classReflection = $propertyReflection->getDeclaringClass();
 
         if ($this->shouldSkipClass($classReflection)) {
-            return true;
+            return false;
         }
 
         if (!$propertyReflection->isPrivate()) {
-            return true;
+            return false;
         }
 
         if (!$propertyReflection->isStatic()) {
-            return true;
+            return false;
         }
 
-        return str_contains((string) $propertyReflection->getDocComment(), '@internal');
+        return !str_contains((string) $propertyReflection->getDocComment(), '@internal');
     }
 }
