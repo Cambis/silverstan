371f24f849cf2609e067da5cac3fc7c153ba948d
Cameron Bryers <12287346+Cambis@users.noreply.github.com>
2025-01-06 11:51:35 +1300

    :sparkles: feat: add ability to alter class manifest

--- src/ClassManifest/ClassManifest.php
+++ src/ClassManifest/ClassManifest.php
@@ -18,37 +18,54 @@ use function array_key_exists;
 use function array_keys;
 use function strtolower;
 
-final readonly class ClassManifest
+final class ClassManifest
 {
     /**
      * A map of lowercase class names to proper class names.
      *
-     * @api
-     *
      * @var array<lowercase-string, class-string>
      */
-    public array $classes;
+    private array $classes;
 
-    private ClassMap $classMap;
+    private readonly ClassMap $classMap;
 
     public function __construct(
-        private ClassMapGenerator $classMapGenerator,
+        private readonly ClassMapGenerator $classMapGenerator,
         /**
          * A list of classes that should be excluded from the manifest.
          *
          * @var list<class-string>
          */
-        private array $excludedClasses,
-        private FileCleaner $fileCleaner,
-        private FileFinder $fileFinder,
-        private NameResolver $nameResolver,
-        private NodeFinder $nodeFinder,
-        private bool $includeTestOnly,
-        private Parser $parser,
-        private TestOnlyFinderVisitor $testOnlyFinderVisitor,
+        private readonly array $excludedClasses,
+        private readonly FileCleaner $fileCleaner,
+        private readonly FileFinder $fileFinder,
+        private readonly NameResolver $nameResolver,
+        private readonly NodeFinder $nodeFinder,
+        private readonly bool $includeTestOnly,
+        private readonly Parser $parser,
+        private readonly TestOnlyFinderVisitor $testOnlyFinderVisitor,
     ) {
         $this->classMap = $this->generateClassMap();
         $this->classes = $this->generateClasses();
+
+        // Register `Page` if it does not exist
+        if (!$this->hasClass('Page')) {
+            $this->addClass('Page', __DIR__ . '/../../stubs/Page.php');
+        }
+
+        // Register `PageController` if it does not exist
+        if (!$this->hasClass('PageController')) {
+            $this->addClass('PageController', __DIR__ . '/../../stubs/PageController.php');
+        }
+
+        // Remove excluded classes
+        foreach ($this->excludedClasses as $excludedClass) {
+            if (!$this->hasClass($excludedClass)) {
+                continue;
+            }
+
+            $this->removeClass($excludedClass);
+        }
     }
 
     /**
@@ -57,6 +74,56 @@ final readonly class ClassManifest
     public function hasClass(string $className): bool
     {
         return array_key_exists(strtolower($className), $this->classes);
+    }
+
+    /**
+     * @param class-string $className
+     * @param non-empty-string $path
+     */
+    public function addClass(string $className, string $path): static
+    {
+        if (!$this->classMap->hasClass($className)) {
+            $this->classMap->addClass($className, $path);
+        }
+
+        $this->classes[strtolower($className)] = $className;
+
+        if ($this->includeTestOnly) {
+            return $this;
+        }
+
+        foreach ($this->findTestOnlyClasses($path) as $testOnlyClass) {
+            if (!$this->hasClass($testOnlyClass)) {
+                continue;
+            }
+
+            $this->removeClass($testOnlyClass);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param class-string $className
+     */
+    public function removeClass(string $className): static
+    {
+        unset($this->classMap->map[$className]);
+        unset($this->classes[strtolower($className)]);
+
+        return $this;
+    }
+
+    /**
+     * A map of lowercase class names to proper class names.
+     *
+     * @api
+     *
+     * @return array<lowercase-string, class-string>
+     */
+    public function getClasses(): array
+    {
+        return $this->classes;
     }
 
     /**
@@ -76,75 +143,60 @@ final readonly class ClassManifest
             $this->fileFinder->getPhpFiles()
         );
 
-        $classMap = $this->classMapGenerator->getClassMap();
+        return $this->classMapGenerator->getClassMap();
+    }
 
-        // Register `Page` if it does not exist
-        if (!$classMap->hasClass('Page')) {
-            $classMap->addClass('Page', __DIR__ . '/../../stubs/Page.php');
+    /**
+     * Get a list of classes in a file that implement `SilverStripe\Dev\TestOnly`.
+     *
+     * @return list<class-string>
+     */
+    private function findTestOnlyClasses(string $path): array
+    {
+        // Strip out all unecessary content from the file
+        $contents = $this->fileCleaner->cleanFile($path);
+        $stmts = $this->parser->parse($contents) ?? [];
+
+        if ($stmts === []) {
+            return [];
         }
 
-        // Register `PageController` if it does not exist
-        if (!$classMap->hasClass('PageController')) {
-            $classMap->addClass('PageController', __DIR__ . '/../../stubs/PageController.php');
-        }
+        $testOnlyClasses = [];
 
-        // Remove excluded classes
-        foreach ($this->excludedClasses as $excludedClass) {
-            if (!$classMap->hasClass($excludedClass)) {
+        // Create a new traverser and register our visitors
+        $traverser = new NodeTraverser();
+        $traverser->addVisitor($this->nameResolver);
+        $traverser->addVisitor($this->testOnlyFinderVisitor);
+
+        $stmts = $traverser->traverse($stmts);
+
+        // Grab all class like nodes
+        $classLikes = $this->nodeFinder->findInstanceOf($stmts, ClassLike::class);
+
+        // Iterate over nodes and check for implementors of `SilverStripe\Dev\TestOnly`
+        foreach ($classLikes as $classLike) {
+            if (!$classLike instanceof ClassLike) {
                 continue;
             }
 
-            unset($classMap->map[$excludedClass]);
-        }
-
-        // No further processing is needed, return
-        if ($this->includeTestOnly) {
-            return $classMap;
-        }
-
-        // Iterate over class map and remove implementors of `SilverStripe\Dev\TestOnly`
-        foreach ($classMap->map as $path) {
-            // Strip out all unecessary content from the file
-            $contents = $this->fileCleaner->cleanFile($path);
-
-            $stmts = $this->parser->parse($contents) ?? [];
-
-            if ($stmts === []) {
+            // Class is missing namespaced name, skip
+            if ($classLike->namespacedName === null) {
                 continue;
             }
 
-            // Create a new traverser and register our visitors
-            $traverser = new NodeTraverser();
-            $traverser->addVisitor($this->nameResolver);
-            $traverser->addVisitor($this->testOnlyFinderVisitor);
-
-            $stmts = $traverser->traverse($stmts);
-
-            // Grab all class like nodes
-            $classLikes = $this->nodeFinder->findInstanceOf($stmts, ClassLike::class);
-
-            // Iterate over nodes and check for implementors of `SilverStripe\Dev\TestOnly`
-            foreach ($classLikes as $classLike) {
-                if (!$classLike instanceof ClassLike) {
-                    continue;
-                }
-
-                // Class is missing namespaced name, skip
-                if ($classLike->namespacedName === null) {
-                    continue;
-                }
-
-                // Class is not implementor of `SilverStripe\Dev\TestOnly`, skip
-                if (!$classLike->hasAttribute(TestOnlyFinderVisitor::ATTRIBUTE_KEY)) {
-                    continue;
-                }
-
-                // Remove implementor of `SilverStripe\Dev\TestOnly` from the class map
-                unset($classMap->map[$classLike->namespacedName->toString()]);
+            // Class is not implementor of `SilverStripe\Dev\TestOnly`, skip
+            if (!$classLike->hasAttribute(TestOnlyFinderVisitor::ATTRIBUTE_KEY)) {
+                continue;
             }
+
+            /** @var class-string $className */
+            $className = $classLike->namespacedName->toString();
+
+            // Add implementor of `SilverStripe\Dev\TestOnly` to list
+            $testOnlyClasses[] = $className;
         }
 
-        return $classMap;
+        return $testOnlyClasses;
     }
 
     /**
