fcd10574b33952c3e735bdda35de935cced97cf4
Cameron Bryers <12287346+Cambis@users.noreply.github.com>
2025-03-05 14:32:08 +1300

    :sparkles: feat: update linting scripts

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   '*.php': [
     'php vendor/bin/parallel-lint --colors --blame',
+    'php vendor/bin/rector process --ansi',
     'php vendor/bin/ecs check --fix --ansi',
   ],
   '!(e2e/*/composer.json)composer.json': [
