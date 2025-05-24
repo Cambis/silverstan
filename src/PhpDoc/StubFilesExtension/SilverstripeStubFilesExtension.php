549670764d779be6776975356a6a34e1fb6f17c8
Cameron Bryers <12287346+Cambis@users.noreply.github.com>
2025-05-24 17:38:12 +1200

    :sparkles: feat: update stub files (#14)
    - Add stubs for ContentController and FieldList
    - Simplify SilverstripeStubFilesExtension
    
    Signed-off-by: Cameron Bryers <12287346+Cambis@users.noreply.github.com>

--- src/PhpDoc/StubFilesExtension/SilverstripeStubFilesExtension.php
+++ src/PhpDoc/StubFilesExtension/SilverstripeStubFilesExtension.php
@@ -7,41 +7,19 @@ namespace Cambis\Silverstan\PhpDoc\StubFilesExtension;
 use Override;
 use PHPStan\PhpDoc\StubFilesExtension;
 use Symfony\Component\Finder\Finder;
-use function array_values;
 
-/**
- * Inspired by https://github.com/larastan/larastan/blob/2.x/src/LarastanStubFilesExtension.php
- */
 final class SilverstripeStubFilesExtension implements StubFilesExtension
 {
-    /**
-     * @var string[]
-     */
-    private const COMMON_STUBS = [
-        __DIR__ . '/../../../stubs/Psr',
-        __DIR__ . '/../../../stubs/SilverStripe/Config',
-        __DIR__ . '/../../../stubs/SilverStripe/Control',
-        __DIR__ . '/../../../stubs/SilverStripe/Core',
-        __DIR__ . '/../../../stubs/SilverStripe/Dev',
-        __DIR__ . '/../../../stubs/SilverStripe/includes',
-        __DIR__ . '/../../../stubs/SilverStripe/ORM',
-        __DIR__ . '/../../../stubs/SilverStripe/Security',
-        __DIR__ . '/../../../stubs/SilverStripe/Versioned',
-        __DIR__ . '/../../../stubs/SilverStripe/VersionedAdmin',
-    ];
-
     #[Override]
     public function getFiles(): array
     {
         $files = [];
-        $stubDirs = [...self::COMMON_STUBS];
-
-        $stubFiles = Finder::create()->files()->name('*.stub')->in($stubDirs);
+        $stubFiles = Finder::create()->files()->name('*.stub')->in(__DIR__ . '/../../../stubs');
 
         foreach ($stubFiles as $stubFile) {
-            $files[$stubFile->getRelativePathname()] = $stubFile->getRealPath();
+            $files[] = $stubFile->getRealPath();
         }
 
-        return array_values($files);
+        return $files;
     }
 }
