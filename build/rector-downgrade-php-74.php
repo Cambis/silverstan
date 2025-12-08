5c420b8eb1c1c4053ce65e56a7c02f7f36883f71
Cameron Bryers <12287346+Cambis@users.noreply.github.com>
2025-12-09 09:37:47 +1300

    :bug: fix: temp skip DowngradeArraySpreadRector

--- build/rector-downgrade-php-74.php
+++ build/rector-downgrade-php-74.php
@@ -3,7 +3,13 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\DowngradePhp74\Rector\Array_\DowngradeArraySpreadRector;
 
 return RectorConfig::configure()
     ->withImportNames(removeUnusedImports: true)
-    ->withDowngradeSets(php74: true);
+    ->withDowngradeSets(php74: true)
+    ->withSkip([
+        // TODO: Weird edge case malforms DisallowMethodCallOnUnsafeDataObjectRule::DEFAULT_ALLOWED_METHOD_CALLS.
+        // Skip everywhere incase it affects other classes.
+        DowngradeArraySpreadRector::class,
+    ]);
